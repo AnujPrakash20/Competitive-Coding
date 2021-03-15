e5c8aa836eb0f1d14d0336e7d72315c98fccdde4
AnujPrakash20 <67551589+AnujPrakash20@users.noreply.github.com>
2021-03-15 13:28:00 +0530

    Add files via upload

---
+++ Dutch flag problem.py
@@ -0,0 +1,24 @@
+# Given an array A[] consisting 0s, 1s and 2s. The task is to write a function that sorts the given array.
+# The functions should put all 0s first, then all 1s and all 2s in last.
+
+def sort012(a, arr_size):
+    lo = 0
+    hi = arr_size - 1
+    mid = 0
+    while mid <= hi:
+        if a[mid] == 0:
+            a[lo], a[mid] = a[mid], a[lo]
+            lo = lo + 1
+            mid = mid + 1
+        elif a[mid] == 1:
+            mid = mid + 1
+        else:
+            a[mid], a[hi] = a[hi], a[mid]
+            hi = hi - 1
+    return a
+
+
+arr = [0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1]
+arr_size = len(arr)
+arr = sort012(arr, arr_size)
+print(arr)
